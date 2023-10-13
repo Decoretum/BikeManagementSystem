22b225544e5059c6a9768592a135af85b841aad7
Gael Estrera <gaelestrera@gmail.com>
2023-10-13 20:02:17 +0800

    Finished info pages

--- frontend/frontend/.next/server/next-font-manifest.js
+++ frontend/frontend/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/Gamadan/Desktop/CSCI180.05Project/frontend/frontend/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
--- frontend/frontend/.next/static/webpack/webpack.4c1702cc18fc6ad2.hot-update.js
+++ frontend/frontend/.next/static/webpack/webpack.4c1702cc18fc6ad2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "34280694f5d0b596"; }
+/******/ 	__webpack_require__.h = function() { return "fd1e5fd2dd4f4c5b"; }
 /******/ }();
 /******/ 
 /******/ }
